81bf95cd82c0f5f3acffa61b9349ca653d942ac1
YooSejeong <nived3@naver.com>
2026-01-05 17:18:03 +0900

    0015

--- src/main/java/com/back/domain/post/service/PostService.java
+++ src/main/java/com/back/domain/post/service/PostService.java
@@ -2,11 +2,11 @@ package com.back.domain.post.service;
 
 import com.back.domain.post.document.Post;
 import com.back.domain.post.repository.PostRepository;
+import com.back.global.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -27,7 +27,7 @@ public class PostService {
         return postRepository.findAll();
     }
 
-    public Optional<Post> findById(String id) {
-        return postRepository.findById(id);
+    public Post findById(String id) {
+        return postRepository.findById(id).orElseThrow(()->new NotFoundException("Post not found with id: " + id));
     }
 }
