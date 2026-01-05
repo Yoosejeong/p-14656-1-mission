f226234b0bfea76ace6d162b082ecffc20f55461
YooSejeong <nived3@naver.com>
2026-01-05 15:37:50 +0900

    0009

---
+++ src/main/java/com/back/domain/post/service/PostService.java
@@ -0,0 +1,11 @@
+package com.back.domain.post.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+    public long count() {
+        return 0;
+    }
+}
